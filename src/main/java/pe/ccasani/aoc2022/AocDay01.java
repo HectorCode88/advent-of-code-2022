28296ae0c1a25c32facc9c3b8c676213eea114bf
Hector Ccasani <hccasani.hc@gmail.com>
2022-12-17 01:31:57 -0500

    #1: Completed Part Two of Day 1

--- src/main/java/pe/ccasani/aoc2022/AocDay01.java
+++ src/main/java/pe/ccasani/aoc2022/AocDay01.java
@@ -1,14 +1,15 @@
 package pe.ccasani.aoc2022;
 
+import static pe.ccasani.aoc2022.Util.readInput;
+
 import hu.akarnokd.rxjava3.math.MathFlowable;
 import hu.akarnokd.rxjava3.operators.FlowableTransformers;
 import io.reactivex.rxjava3.core.Flowable;
-
-import static pe.ccasani.aoc2022.Util.readInput;
+import java.util.Comparator;
 
 public class AocDay01 {
 
-  public static void findTheMostCalories() {
+  public static void PartOne() {
 
     Flowable.fromIterable(readInput("day01-1.txt"))
       .compose(FlowableTransformers.bufferSplit(""::equals))
@@ -16,6 +17,19 @@ public class AocDay01 {
         .map(Integer::valueOf)
         .to(MathFlowable::sumInt))
       .to(MathFlowable::max)
+      .subscribe(System.out::println)
+      .dispose();
+  }
+
+  public static void PartTwo() {
+    Flowable.fromIterable(readInput("day01-1.txt"))
+      .compose(FlowableTransformers.bufferSplit(""::equals))
+      .flatMap(calories -> Flowable.fromIterable(calories)
+        .map(Integer::valueOf)
+        .to(MathFlowable::sumInt))
+      .sorted(Comparator.reverseOrder())
+      .take(3)
+      .to(MathFlowable::sumInt)
       .subscribe(System.out::println)
       .dispose();
   }
